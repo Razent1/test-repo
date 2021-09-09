8a355b3c65a5ffcf080099f207693ee8e824c431
Razent <irzoentul.ilya@gmail.com>
2021-09-09 16:38:36 +0300

    final

--- textstat/index.js
+++ textstat/index.js
@@ -7,9 +7,39 @@
  * 4. Count of words.
  * @param {object} event - Event class
  */
+//const txt = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+function vowels_counter(str) {
+    let v = str.match(/[aeiou]/gi);
+    return v === null ? 0 : v.length;
+}
+
+function consonats_counter(str) {
+    let v = str.match(/([^aeiou!,.])/gi);
+    return v === null ? 0 : v.length;
+}
+
 function onStatisticsClicked(event) {
-    //TODO: Implement this function
-    console.log('onStatisticsClicked called');
+    let count_spaces = 0;
+    for (const l of txt) {
+        if (l === ' ') {
+            count_spaces++;
+        }
+    }
+    let char_count = document.getElementById('valCharCount');
+    char_count.value = txt.length;
+
+    let spaces_count = document.getElementById('valSpacesCount');
+    spaces_count.value = count_spaces;
+
+    let vowels_count = document.getElementById('valVowelsCount');
+    vowels_count.value = vowels_counter(txt);
+
+    let consonants_count = document.getElementById('valConsonantsCount');
+    consonants_count.value = consonats_counter(txt);
+
+    let words_count = document.getElementById('valWordsCount');
+    words_count.value = txt.split(' ').length;
 }
 
 /**
@@ -18,8 +48,16 @@ function onStatisticsClicked(event) {
  * @param {object} event - Event class
  */
 function onRemoveWordsClicked(event) {
-    //TODO: Implement this function
-    console.log('onRemoveWordsClicked called');
+    let text = txt;
+    let words = text.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        if (i % 2 === 0) {
+            words.splice(i, 1);
+        }
+    }
+    let words_count = document.getElementById('valOddWords');
+    words_count.value = words.join(' ');
 }
 
 
